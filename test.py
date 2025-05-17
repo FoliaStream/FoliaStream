abce880aade493dd4662c41b64e066810ebabc7f
FoliaStream <foliastream@gmail.com>
2025-05-17 07:21:19 +0200

    understand

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-
 st.write("TEST")
 
-
 import sys
-print("Python path:", sys.path)
+st.write("Python path:", sys.path)
 
 try:
     import yaml
-    print("YAML module loaded from:", yaml.__file__)
+    st.write("YAML module loaded from:", yaml.__file__)
 except ImportError as e:
-    print("YAML import failed:", e)
+    st.write("YAML import failed:", e)
+
