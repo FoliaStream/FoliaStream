13abdbcf376996a0d36adb48c9b349392919ce57
FoliaStream <foliastream@gmail.com>
2025-05-17 18:37:24 +0200

    pretty

--- HOME.py
+++ HOME.py
@@ -5,37 +5,40 @@ import yaml
 import os
 from pipe.streamain import main
 
-options_country = ['AUS', 'DNK', 'DEU', 'BGD', 'BRA', 'CAN', 'CHN', 'IND', 'IDN', 'JPN', 'MYS', 'MEX', 'NOR', 'PAK', 'KOR', 'LKA', 'GBR', 'USA', 'KAZ', 'KWT', 'MOZ', 'QAT', 'SAU', 'ZAF', 'THA', 'ARE', 'VNM', 'SWE', 'GRC', 'AUT', 'HRV', 'BGR', 'ESP', 'FRA', 'ITA', 'POL', 'CZE', 'SVK', 'HUN', 'IRL', 'ISR', 'MAR', 'DZA', 'ROU', 'NLD']
-options_year = [2020, 2021, 2022, 2023, 2024]
-options_sector = ["electricity-generation","cement","aluminum","pulp-and-paper","chemicals","domestic-aviation","international-aviation","oil-and-gas-refining","coal-mining","bauxite-mining","iron-mining","copper-mining","net-forest-land","net-wetland","net-shrubgrass","cropland-fires"]
+options_country = ['Select country','AUS', 'DNK', 'DEU', 'BGD', 'BRA', 'CAN', 'CHN', 'IND', 'IDN', 'JPN', 'MYS', 'MEX', 'NOR', 'PAK', 'KOR', 'LKA', 'GBR', 'USA', 'KAZ', 'KWT', 'MOZ', 'QAT', 'SAU', 'ZAF', 'THA', 'ARE', 'VNM', 'SWE', 'GRC', 'AUT', 'HRV', 'BGR', 'ESP', 'FRA', 'ITA', 'POL', 'CZE', 'SVK', 'HUN', 'IRL', 'ISR', 'MAR', 'DZA', 'ROU', 'NLD']
+options_year = ['Select year', 2020, 2021, 2022, 2023, 2024]
+options_sector = ['Select sector',"electricity-generation","cement","aluminum","pulp-and-paper","chemicals","domestic-aviation","international-aviation","oil-and-gas-refining","coal-mining","bauxite-mining","iron-mining","copper-mining","net-forest-land","net-wetland","net-shrubgrass","cropland-fires"]
 
-country = st.selectbox("Country", options=options_country)
-year = st.selectbox("Year", options=options_year, index = 4)
-sector = st.selectbox("Sector", options=options_sector)
-capture_cost = st.number_input("Capture cost", step=1)
-emission_cost = st.number_input("Emission cost", step=1)
-
+country = st.selectbox("Country", options=options_country, placeholder="Select country")
+year = st.selectbox("Year", options=options_year, placeholder="Select year")
+sector = st.selectbox("Sector", options=options_sector, placeholder='Select sector')
+capture_cost = st.number_input("Capture cost", step=1, value=0)
+emission_cost = st.number_input("Emission cost", step=1, value=0)
 
 
 with st.sidebar:
     st.image(f"{os.getcwd()}/logo.png")
 
+if country != 'Select country' and year != 'Select year' and sector != 'Select sector':
 
-if st.button("RUN"):
-    with open(f'{os.getcwd()}/pipe/config/case.yaml', "w") as f:
+    if st.button("RUN"):
+        with open(f'{os.getcwd()}/pipe/config/case.yaml', "w") as f:
 
-        data = {
+            data = {
 
-            "country" : country,
-            "year" : int(year),
-            "sector" : sector,
-            "capture_cost" : capture_cost,
-            "emission_cost" : emission_cost
-        }
-        yaml.dump(data, f, default_flow_style=False)
+                "country" : country,
+                "year" : int(year),
+                "sector" : sector,
+                "capture_cost" : capture_cost,
+                "emission_cost" : emission_cost
+            }
+            yaml.dump(data, f, default_flow_style=False)
 
-    main()
+        main()
 
-    st.dataframe(pd.read_csv(str(f"{os.getcwd()}/output/final/csv/{country}__{year}__{sector}/network_results.csv")))
-    map = open(str(f"{os.getcwd()}/output/final/fig/{country}__{year}__{sector}/network_map_out.html"))
-    st.components.v1.html(map.read(), height=500, scrolling=True)
+        st.dataframe(pd.read_csv(str(f"{os.getcwd()}/output/final/csv/{country}__{year}__{sector}/network_results.csv")))
+        map = open(str(f"{os.getcwd()}/output/final/fig/{country}__{year}__{sector}/network_map_out.html"))
+        st.components.v1.html(map.read(), height=500, scrolling=True)
+
+else:
+    st.button("RUN", disabled=True)
