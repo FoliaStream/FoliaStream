1cc0c209a5131542d073bea219c0c19323c38c18
FoliaStream <foliastream@gmail.com>
2025-05-17 07:54:21 +0200

    test_working

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 import streamlit as st
+import yaml 
 
 st.write("TEST")
-
-import sys
-st.write("Python path:", sys.path)
-
-try:
-    import yaml
-    st.write("YAML module loaded from:", yaml.__file__)
-except ImportError as e:
-    st.write("YAML import failed:", e)
-
