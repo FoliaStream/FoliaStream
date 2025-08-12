6d11c1178d5a2cec62a451e92dda80668717eec9
FoliaStream <foliastream@gmail.com>
2025-08-12 12:04:45 +0200

    test

--- pages/NETWORK_MAP.py
+++ pages/NETWORK_MAP.py
@@ -194,7 +194,7 @@ if clicked and clicked.get("last_active_drawing") is not None:
                 st.subheader("Network Map:")
 
                 map_network = open(str(f"{os.getcwd()}/output/final/fig/{country}__{year}__{sector}/network_map_out.html"))
-                st.components.v1.html(map_network.read(),height=500, scrolling=True)
+                st.components.v1.html(map_network.read(),height=500, scrolling=True, width=None)
 
         else:
             st.button("RUN", disabled=True)
